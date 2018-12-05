e8bac1742b546ccee962f90cbb7efab074b72cff
saran <saranbs2013@gmail.com>
2018-12-06 07:54:53 +0800

    Elvis, Strong, Safe cast operators

--- src/kt/nullability/nullability.kt
+++ src/kt/nullability/nullability.kt
@@ -1,10 +1,12 @@
 package kt.nullability
 
+import jdk.nashorn.internal.objects.NativeString.substring
+
 fun main(args: Array<String>){
     println("Hello World")
 
     /*********************************Safe call operator***********************************/
-    val abc:String? = null
+    var abc:String? = null
 
     //Usual java way
     if(abc!=null){
@@ -48,4 +50,93 @@ fun main(args: Array<String>){
 
     println(t)
 
+
+    /*********************************Safe call with let***********************************/
+
+    //If we want to pass a non null object to a method in a library, we can use ?.let{}
+
+    abc = null
+
+    //Java style
+    /*
+    if(abc!=null){
+        printText(abc)
+    }*/
+
+    abc?.let{
+        printText(it)
+    }
+    //Here we are executing code only if value of abc is not null
+
+    /*********************************Safe cast operator***********************************/
+
+    var xyz: String?
+
+    xyz = null
+
+    println("Java Style")
+    if(xyz is String){
+        println(xyz.substring(0,3))
+    }else{
+        println(null)
+    }
+
+    println("Kotlin Style")
+
+    //Equivalent expression using Kotlin safe cast call
+    println((xyz as? String)?.substring(0,3))
+    //if xyz is null, entire expression is shorted to null, without causing null pointer expression
+
+
+    /*********************************Elvis  operator***********************************/
+
+    var qrs:String?
+
+    qrs = "123"
+
+/*    println("Java Style")
+    if(qrs==null){
+        qrs = "Hello World"
+    }else{
+        qrs = qrs
+    }
+    println(qrs) */
+
+    qrs = qrs?:"Hello World"
+    println(qrs)
+
+    /*********************************Equals safe operator***********************************/
+
+    var bb:String? = null
+    val tt = "sdfsd"
+
+    println("bb==tt: ${bb==tt}")
+    //Equals operator can be used with nullable object, since it is a safe operator
+
+    /*********************************Strong (non null asserted)  operator***********************************/
+
+    var pq:String?
+
+    pq = null
+
+    //Java way
+ /*   if(pq==null){
+        throw Exception()
+    }else{
+        pq.substring(0,1)
+    }*/
+
+//    println(pq!!.substring(0,1))
+
+    val temp = pq!!
+
+    println(temp)
+
+    //If we use !! operator and the object happens to be null, exception will be thrown where
+    //we used !! (line:107) and not where we used the null object
+
+}
+
+fun printText(str:String){
+    println("In printText: $str")
 }
