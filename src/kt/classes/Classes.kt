899e613643cf9f3d818fa999beafcd78c4b6b9c1
saran <saranbs2013@gmail.com>
2019-01-07 08:49:45 +0800

    Custom getter/setter

--- src/kt/classes/Classes.kt
+++ src/kt/classes/Classes.kt
@@ -5,6 +5,10 @@ fun main(args:Array<String>){
     val b = B()
     val d = D5("Saran")
     var e = D6("Saran")
+    var f = D8("Saran",31)
+
+    println(f.firstName+f.age+f.isAdult)
+    f.age =32
 }
 /*By default Kotlin classes are public & final*/
 
@@ -64,4 +68,29 @@ class D5{
         this.fullTime = fTime
     }
 
+}
+
+//Non public member variable => internal
+class D7(val firstName:String,internal var fullTime:Boolean = true)
+
+//For all non private params mentioned in primary constuctor, kt will treat it as properties & auto generate getters and setters
+//If you need custom getters/setters, you cant declare it as properties (val/var) in primary constructor, it should be
+//inside class
+
+class D8(val firstName:String,age:Int){ //age:Int => not property
+    var age = age //declared in class. Custom get/set should happen immediately after this line
+    get(){
+        println("Returning age"+field)
+        return field
+    }
+    set(value){
+        field = value
+        println("setting age"+field)
+    }
+
+    var isAdult = false //declared in class
+    get(){//declared in class. Custom get/set should happen immediately after this line
+        println("Returning isAdult")
+        return age>18
+    }
 }
